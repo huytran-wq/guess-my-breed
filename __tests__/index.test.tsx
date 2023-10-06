950b309fd00b8f5071c09cd83d3b128866229cc9
Huy Tran <huy.tran@appfolio.com>
2023-10-06 01:26:10 -0700

    Update the index page to use the GuessForm component

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,17 +1,15 @@
-/**
- * @jest-environment jsdom
- */
-import { render, screen } from '@testing-library/react'
-import Home from '@/pages/index'
+import { render, screen } from "@testing-library/react";
+import Home from "../pages/index";
+import "@testing-library/jest-dom";
 
-describe('Home', () => {
-  it('renders a heading', () => {
-    render(<Home />)
+describe("Home", () => {
+  it("renders a heading", () => {
+    render(<Home />);
 
-    const heading = screen.getByRole('heading', {
-      name: /welcome to next\.js!/i,
-    })
+    const heading = screen.getByRole("heading", {
+      name: /guess my breed/i,
+    });
 
-    expect(heading).toBeInTheDocument()
-  })
-})
+    expect(heading).toBeInTheDocument();
+  });
+});
